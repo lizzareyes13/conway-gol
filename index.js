bd7da61cb3710cad58992b55c234351f270ab57b
Eric Phillips <dephil29@hotmail.com>
2017-10-03 19:21:55 -0400

    added mouse events for the drawing

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "#0dff00";
+
+getElementById("canvas").mousedown(function(event) {
+    lastMouseEvent = event;
+    isMouseDown = true;
+});
+
+getElementById("canvas").mousemove(function(currentMouseEvent) {
+    if(isMouseDown === true) {
+      // ctx.lineJoin = 'round';
+      // ctx.lineCap = 'round';
+        context.beginPath();
+        context.moveTo(lastMouseEvent.offsetX, lastMouseEvent.offsetY);
+        context.lineTo(currentMouseEvent.offsetX, currentMouseEvent.offsetY);
+        context.strokeStyle = "#0dff00";
+        context.lineWidth = "4px";
+        context.stroke();
+        console.log(lastMouseEvent.offsetX, currentMouseEvent.offsetX);
+        lastMouseEvent = currentMouseEvent;
+    }
+});
+
+getElementById("canvas").mouseup(function() {
+    isMouseDown = false;
+});
