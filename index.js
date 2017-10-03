d92dc1fb0770f558f3c97b54949d80fa28969e1f
Lizza Reyes <lizzareyes13@gmail.com>
2017-10-03 19:21:35 -0400

    grid created

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#0dff00";
+
+
+// creating the grid
+function createArray (rows) {
+  var array = [];
+
+  for(var i = 0; i < rows; i++) {
+    array[i] = [];
+  }
+  return array;
+}
