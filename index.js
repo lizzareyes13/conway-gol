b9523fae174d32c77c0654bfe05dbe803a8039d5
Lizza Reyes <lizzareyes13@gmail.com>
2017-10-03 20:01:47 -0400

    grid population

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ function createArray (rows) {
   }
   return array;
 }
+
+// var grid = createArray(gridWidth);
+//
+// grid;
+
+//populating the grid
+function fillRandom () {
+  for (var j = 0; j < gridHeight; j++) {
+    for (var k = 0; k < gridWidth; k++) {
+      var rawRandom = Math.random();
+      var improvedNum = (rawRandom * 2);
+      var randomBinary = Math.floor(improvedNum);
+      if (randomBinary === 1) {
+        theGrid[j][k] = 1;
+      }else{
+        theGrid[j][k] = 0;
+      }
+    }
+  }
+}
